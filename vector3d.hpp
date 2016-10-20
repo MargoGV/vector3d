440cecbf621ff3129db1eca80fe28638e1ea948f
MargoGV <MV.Gubina@yandex.ru>
2016-10-20 16:32:04 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -5,13 +5,24 @@
 
 class vector3d {
 public:
+    int x;
+    int y;
+    int z;
+
+
     const static vector3d X;
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
-
-    // TODO: дописать
-
+    vector3d();
+    vector3d(int x ,int y, int z);
+    int getX();
+    int getY();
+    int getZ();
+    vector3d operator+(vector3d a);
+    vector3d operator-(vector3d a);
+    vector3d operator*(vector3d a);
+    int vector3d::lenght(vector3d a);
 };
 
 // TODO: дописать
