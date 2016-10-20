79b6557ca788562abc783bf37ff24480d45beba3
MargoGV <MV.Gubina@yandex.ru>
2016-10-20 16:31:41 +0400

    Update vector3d.cpp

--- vector3d.cpp
+++ vector3d.cpp
@@ -1,30 +1,39 @@
-#ifndef VECTOR3D_H
-#define VECTOR3D_H
+#include "vector3d.hpp"
 
-#include <ostream>
+#include <cmath>
 
-class vector3d {
-public:
-    int x;
-    int y;
-    int z;
+const vector3d vector3d::X = {1, 0, 0};
+const vector3d vector3d::Y = {0, 1, 0};
+const vector3d vector3d::Z = {0, 0, 1};
+const vector3d vector3d::ZERO = {0, 0, 0};
+vector3d::vector3d() :x(0),y(0),z(0) {}
+vector3d::vector3d(int x, int y, int z) :x(x),y(y),z(z) {}
+int vector3d::getX() {return x; }
+int vector3d::getZ() {return z; }
+int vector3d::getY() {return y; }
 
-
-    const static vector3d X;
-    const static vector3d Y;
-    const static vector3d Z;
-    const static vector3d ZERO;
-    vector3d();
-    vector3d(int x ,int y, int z);
-    int getX();
-    int getY();
-    int getZ();
-    vector3d operator+(vector3d a);
-    vector3d operator-(vector3d a);
-    vector3d operator*(vector3d a);
-    int vector3d::lenght(vector3d a);
-};
-
-// TODO: дописать
-
-#endif
+vector3d vector3d::operator+(vector3d a)
+{
+    int resx=a.getX()+ getX();
+    int resy=a.getY()+ getY();
+    int resz=a.getZ()+ getZ();
+    return vector3d(resx, resy, resz);
+}
+vector3d vector3d::operator-(vector3d a)
+{
+    int resx= getX()- a.getX();
+    int resy= getY()- a.getY();
+    int resz= getZ()- a.getZ();
+    return vector3d(resx, resy, resz);
+}
+vector3d vector3d::operator*(vector3d a)
+{
+    int resx= getX()*a.getX();
+    int resy= getY()*a.getY();
+    int resz= getZ()*a.getZ();
+    return vector3d(resx, resy, resz);
+}
+int vector3d::lenght(vector3d a)
+{
+    return std::sqrt(a.x*a.x+a.y*a.y+a.z*a.z);
+}
