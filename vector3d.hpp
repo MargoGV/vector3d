2a10278832acc58dfd59e92c287fdd05f797a1cd
MargoGV <MV.Gubina@yandex.ru>
2016-11-03 16:26:00 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -6,9 +6,9 @@
 
 class vector3d {
 public:
-    int x;
-    int y;
-    int z;
+    double x;
+    double y;
+    double z;
 
 
     const static vector3d X;
@@ -16,7 +16,7 @@ public:
     const static vector3d Z;
     const static vector3d ZERO;
     vector3d();
-    vector3d(int x ,int y, int z);
+    vector3d(double x ,double y, double z);
     vector3d operator+(const vector3d a)const;
     vector3d operator-(const vector3d a)const;
     double operator*(const vector3d a)const;
@@ -30,5 +30,7 @@ public:
 };
 
 vector3d operator*(int b, const vector3d a);
+std::ostream &operator<<(std::ostream &stream, const vector3d &a);
 #endif
+
 
